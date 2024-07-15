61da036d7e2465eedfdc6ec35b71a7a1634cf3cc
HarenzEscat <131321445+HarenzEscat@users.noreply.github.com>
2024-07-15 11:34:37 +0800

    aaa

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+
+//student
+Route::get('/student-evaluation-consultation', function () {
+    return view('student.student-evaluation-consultation');
+})->name('student-evaluation');
+
+Route::get('/evaluation', function () {
+    return view('student.evaluation');
+})->name('evaluation');
+
+Route::get('/consultation', function () {
+    return view('student.consultation');
+})->name('consultation');
