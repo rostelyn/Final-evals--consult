0a6a2141aee82cc70cd025725d883823420c8f76
rostelyn <rostelynjanea@gmail.com>
2024-07-17 10:08:05 +0800

    update hr db

--- resources/views/hr/hr-db.blade.php
+++ resources/views/hr/hr-db.blade.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HR DASHBOARD</title>
-</head>
-<body>
-    <h1>You are in HR dashboard</h1>
-</body>
-</html>
-
 @extends('layouts.hr-layout')
 
 @section('content')
