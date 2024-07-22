161230a5b1d1f51b3fe6d47567923e59b20b2ece
HarenzEscat <131321445+HarenzEscat@users.noreply.github.com>
2024-07-22 09:41:05 +0800

    DESIGN

--- resources/views/student/evaluation/faculty/show.blade.php
+++ resources/views/student/evaluation/faculty/show.blade.php
@@ -1,35 +1,27 @@
 @extends('layouts.evaluation-layout')
 
 @section('content')
-<style>
-    .btn-custom {
-        background-color: #62CB6D;
-        border-color: #62CB6D;
-        color: white;
-    }
-    .btn-custom:hover {
-        background-color: #52b25d;
-        border-color: #52b25d;
-    }
-</style>
 
 <div class="container mt-5">
     <div class="row">
         <div class="col-12">
             <h2 class="text-center mb-4">{{ $department }} Department</h2>
-            <div class="department-head mb-4 p-3 bg-light rounded">
+            <div class="department-head faculty-container">
+                <div class="picture mb-3">
+                    <!-- Placeholder for picture -->
+                </div>
                 <h3>Department Head: {{ $details['head'] }}</h3>
                 <a href="{{ route('evaluation-form') }}" class="btn btn-custom mt-2">Evaluate</a>
             </div>
-            <div class="row">
+            <div class="row justify-content-center">
                 @foreach($details['members'] as $member)
-                    <div class="col-md-6 mb-4">
-                        <div class="faculty-member p-3 bg-white rounded shadow-sm">
-                            <div class="picture mb-3">
-                                <!-- Placeholder for picture -->
+                    <div class="col-md-4 col-lg-3 mb-4">
+                        <div class="faculty-container">
+                            <div class="faculty-member">
+                                <div class="picture"></div>
+                                <p>{{ $member }}</p>
+                                <a href="{{ route('evaluation-form') }}" class="btn btn-custom mt-2">Evaluate</a>
                             </div>
-                            <p>{{ $member }}</p>
-                            <a href="{{ route('evaluation-form') }}" class="btn btn-custom mt-2">Evaluate</a>
                         </div>
                     </div>
                 @endforeach
