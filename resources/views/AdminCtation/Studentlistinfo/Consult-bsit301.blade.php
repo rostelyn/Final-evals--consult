3f338a3df0351cec8f1dfc3578e4e46bde9616e0
kairo01 <147585990+kairo01@users.noreply.github.com>
2024-07-26 01:23:23 +0800

    CLEARING SOME ERROR

--- resources/views/AdminCtation/Studentlistinfo/Consult-bsit301.blade.php
+++ resources/views/AdminCtation/Studentlistinfo/Consult-bsit301.blade.php
@@ -4,22 +4,4 @@
 @section('content')
     <h1>STUDENT EVALUATION AND CONSULTATION</h1>
     <h2>BSIT 301</h2>
-    <div>
-        <p>STUDENT NAME</p>
-        <a href="{{ ('') }}">
-            <button>VIEW STUDENT</button>
-        </a>
-    </div>
-    <div>
-        <p>STUDENT NAME</p>
-        <a href="{{ route('') }}">
-            <button>VIEW STUDENT</button>
-        </a>
-    </div>
-    <div>
-        <p>STUDENT NAME</p>
-        <a href="{{ route('') }}">
-            <button>VIEW STUDENT</button>
-        </a>
-    </div>
 @endsection
