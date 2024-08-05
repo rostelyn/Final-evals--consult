00a90434886b12a5f7bf88259858bcd3270317d2
rostelyn <rostelynjanea@gmail.com>
2024-08-05 18:50:27 +0800

    change the icon to logo

--- resources/views/hr/hr-pick.blade.php
+++ resources/views/hr/hr-pick.blade.php
@@ -10,19 +10,19 @@
     <title>Student EVALUATION AND CONSULTATION</title>
 </head>
 <body>
-        <div class="title">STUDENT EVALUATION AND CONSULTATION</div>
-            <div class="main-container">
-                 <div class="courses-container">
+    <div class="title">STUDENT EVALUATION AND CONSULTATION</div>
+    <div class="main-container">
+        <div class="courses-container">
             <button class="course-button college">
-                <div class="course-icon">🎓</div>
+                <img class="course-icon" src="{{ asset('css/logoo.jpg') }}" alt="College Department Icon">
                 <div>COLLEGE DEPARTMENT</div>
-        </button>
-        <button class="course-button highschool">
-            <div class="course-icon">👨‍🎓</div>
-            <div>HIGH SCHOOL</div>
-        </button>
-             </div>
+            </button>
+            <button class="course-button highschool">
+                <img class="course-icon" src="{{ asset('css/hslogo.jpg') }}" alt="High School Icon">
+                <div>HIGH SCHOOL</div>
+            </button>
         </div>
+    </div>
 </body>
 </html>
 @endsection
