96a3084a4cef1d14172ad3b98540f06cdc4f991b
rostelyn <rostelynjanea@gmail.com>
2024-08-07 22:25:09 +0800

    change border to shadow

--- resources/views/hr/hr-pick.blade.php
+++ resources/views/hr/hr-pick.blade.php
@@ -7,18 +7,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/pick.css') }}">
-    <title>Student EVALUATION SUDENT LIST</title>
+    <title>Student Evaluation and Consultation</title>
 </head>
 <body>
-        <div class="title">Student EVALUATION SUDENT LIST</div>
+        <div class="title">Student Evaluation and Consultation</div>
             <div class="main-container">
                  <div class="courses-container">
             <div class="course-button college" >
-            <img class="course-icon" src="{{ asset('css/logoo.jpg') }}" alt="College Department Icon">
+            <img class="course-icon" src="{{ asset('css/resources/logoo.jpg') }}" alt="College Department Icon">
                 <div><a href="hr-bsit">COLLEGE DEPARTMENT</a></div>    
         </div> 
         <button class="course-button highschool">
-        <img class="course-icon" src="{{ asset('css/hslogo.jpg') }}" alt="High School Icon">
+        <img class="course-icon" src="{{ asset('css/resources/hslogo.jpg') }}" alt="High School Icon">
             <div>HIGH SCHOOL</div>
         </button>
              </div>
