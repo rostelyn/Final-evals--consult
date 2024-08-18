abbf57d6786155546c36d08d68fd8082e348f526
rostelyn <rostelynjanea@gmail.com>
2024-08-18 22:43:25 +0800

    RESOURCES-GENERAL RESOURSES

--- resources/views/AdminCtation/Ct-pick.blade.php
+++ resources/views/AdminCtation/Ct-pick.blade.php
@@ -14,11 +14,11 @@
             <div class="main-container">
                  <div class="courses-container">
             <div class="course-button college" >
-            <img class="course-icon" src="{{ asset('css/resources/logoo.jpg') }}" alt="College Department Icon">
+            <img class="course-icon" src="{{ asset('css/GeneralResources/logoo.jpg') }}" alt="College Department Icon">
                 <div><a href="Ct-student">COLLEGE DEPARTMENT</a></div>    
         </div> 
         <button class="course-button highschool">
-        <img class="course-icon" src="{{ asset('css/resources/hslogo.jpg') }}" alt="High School Icon">
+        <img class="course-icon" src="{{ asset('css/GeneralResources/hslogo.jpg') }}" alt="High School Icon">
                 <div><a href="Ct-studentHS">HIGH SCHOOL</a></div>
         </button>
              </div>
