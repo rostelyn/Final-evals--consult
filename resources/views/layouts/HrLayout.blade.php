fdd9e80300b7c8fc78757572a0ebcf29476a7abc
rostelyn <rostelynjanea@gmail.com>
2024-09-28 01:18:02 +0800

    HRCollege Header

--- resources/views/layouts/HrLayout.blade.php
+++ resources/views/layouts/HrLayout.blade.php
@@ -5,12 +5,16 @@
     
     <title>HR - Student Evaluation and Consultation</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Averia+Serif+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=DM+Serif+Text:ital@0;1&family=Diplomata+SC&display=swap" rel="stylesheet">
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <div class="sidebar" id="sidebar">
         <div class="logo">
-            <img src="{{ asset('css/GeneralResources/logoo.jpg') }}" alt="Logo">
+            <h1>HR Admin</h1>
         </div>
         <ul>
             <li><a href="{{('HrDashboard') }}">Dashboard</a></li>
