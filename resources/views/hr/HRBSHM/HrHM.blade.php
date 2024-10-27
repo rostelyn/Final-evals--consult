38a731e88d1bf1d1d1a22b6dff722a9455729ed3
rostelyn <rostelynjanea@gmail.com>
2024-10-27 13:49:56 +0800

    minimal change for HM

--- resources/views/hr/HRBSHM/HrHM.blade.php
+++ resources/views/hr/HRBSHM/HrHM.blade.php
@@ -16,51 +16,51 @@
     <div class="year-container">
         <div class="year-block">
             <h2>First Year</h2>
-            <a href="{{('HM101') }}">
+            <a href="{{('BSHM101') }}">
                 <button>101</button>
             </a>
-            <a href="{{('HM102') }}">
+            <a href="{{('BSHM102') }}">
                 <button>102</button>
             </a>
 
-            <a href="{{('HM103') }}">
+            <a href="{{('BSHM103') }}">
                 <button>103</button>
             </a>
             
         </div>
         <div class="year-block">
             <h2>Second Year</h2>
-            <a href="{{('HM201') }}">
+            <a href="{{('BSHM201') }}">
                 <button>201</button>
             </a>
-            <a href="{{('HM202') }}">
+            <a href="{{('BSHM202') }}">
                 <button>202</button>
             </a>
-            <a href="{{('HM203') }}">
+            <a href="{{('BSHM203') }}">
                 <button>203</button>
             </a>
         </div>
         <div class="year-block">
             <h2>Third Year</h2>
-            <a href="{{('HM301') }}">
+            <a href="{{('BSHM301') }}">
                 <button>301</button>
             </a>
-            <a href="{{('HM302') }}">
+            <a href="{{('BSHM302') }}">
                 <button>302</button>
             </a>
-            <a href="{{('HM303') }}">
+            <a href="{{('BSHM303') }}">
                 <button>303</button>
             </a>
         </div>
         <div class="year-block">
             <h2>Fourth Year</h2>
-            <a href="{{('HM401') }}">
+            <a href="{{('BSHM401') }}">
                 <button>401</button>
             </a>
-            <a href="{{('HM402') }}">
+            <a href="{{('BSHM402') }}">
                 <button>402</button>
             </a>
-            <a href="{{('HM403') }}">
+            <a href="{{('BSHM403') }}">
                 <button>403</button>
             </a>
         </div>
